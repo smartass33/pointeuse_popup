082e3aa685a849252f468c1983bf5ae43cb62324
smartass33 <henri.martin@gmail.com>
2016-02-06 11:44:51 +0100

    python card manager

--- src/pointeuse/launch.py
+++ src/pointeuse/launch.py
@@ -14,6 +14,9 @@ import tkFont
 import requests
 import json
 import sys
+import tkMessageBox
+#import nxppy
+
 
 class Popup():
     def center(self,win,width,height):
@@ -38,24 +41,26 @@ class Popup():
         print('entering senDate')
         data = {'username':'ali'}
         r = requests.get("http://192.168.1.31/employee/dummy", params = data)
-        #print(r.url)
-        #print(r.text)
         jsonStr = json.loads(r.text)
-        #firstName = jsonStr['firstName']
-        #lastName = jsonStr['lastName']
-        #loggingTime = jsonStr['loggingTime']
-        #inOrOutType = jsonStr['type']
-        #print ('firstName:' +firstName)
-        #status = jsonStr['status']
-        self.display(jsonStr)
+        hasError = (False if str(jsonStr['status'].encode('utf8','ignore')) == 'OK' else True)
+        if (hasError):
+            # il y a une erreur: creation d'un message de warning
+            self.displayWarning(str(jsonStr['status'].encode('utf8','ignore')))
+        else:
+            self.displayPopup(jsonStr)
 
-    def display(self,jsonStr):
+
+    def displayWarning(self,status):
+        root = tk.Tk()
+        root.withdraw()
+        tkMessageBox.showwarning("Probleme de pointage", "Il y a eu un probleme avec le pointage:\n\n"+status)
+
+    def displayPopup(self,jsonStr):
         firstName = jsonStr['firstName']
         lastName = jsonStr['lastName']
         loggingTime = jsonStr['loggingTime']
-        inOrOutType = jsonStr['type']
-        inOrOutType = ('SORTIE' if jsonStr['type'] is'S' else 'ENTREE')
-        isEntry = (False if jsonStr['type'] is'S' else True)
+        inOrOutType = ('SORTIE' if (jsonStr['type'].encode('utf8','ignore')) is'S' else 'ENTREE')
+        isEntry = (False if (jsonStr['type'].encode('utf8','ignore')) is'S' else True)
         root = tk.Tk()
         #root.withdraw()
         root.attributes('-alpha', 0.0)
@@ -70,34 +75,49 @@ class Popup():
         #lab.pack(ipadx=4, padx=4, ipady=4, pady=4, fill='both')
         #mycolor = '#%02x%02x%02x' % (64, 204, 208)  # set your favourite rgb color
         #mycolor2 = '#40E0D0'  # or use hex if you prefer 
-        #backGroundColor = 'greaen'
+        #backGroundColor = 'green'
         
         if (isEntry): 
             backGroundColor = 'green'
+            greeting = 'Bonjour'
         else:
             backGroundColor = 'red'
+            greeting = 'Au revoir'
         
         root.tk_setPalette(background=backGroundColor, foreground='white',
                    activeBackground='black', activeForeground=backGroundColor)
-        customFont = tkFont.Font(family="Helvetica", size=24)
+        customFont = tkFont.Font(family="Arial", size=24)
         text = tk.Text(root, width=20, height=2, font=customFont)
-        text.insert(tk.INSERT, "Bonjour, "+firstName+" "+lastName+"!\n")
+        text.insert(tk.INSERT,greeting+" "+firstName+" "+lastName+"\n\n")
         text.insert(tk.END, inOrOutType+": "+loggingTime+"\n")
         text.pack(expand=1,fill=tk.BOTH)
         
         # adding a tag to a part of text specifying the indices
-        self.center(root,500,400)
+        self.center(root,400,100)
     
         root.attributes('-alpha', 1.0)
+        root.attributes("-topmost", True)
         root.after(5000, lambda: root.destroy())
         root.mainloop()
        # root.deiconify()
 
 def main(args):
     global popup
+    #mifare = nxppy.Mifare()
+    print('starting NFC reading...')
     popup = Popup()
     jsonStr = popup.sendDate()
-    popup.display(jsonStr)
+'''
+    while True:
+        try:
+            uid = mifare.select()
+            print('Read uid: '+uid)
+            popup = Popup()
+            jsonStr = popup.sendDate()
+        except nxppy.SelectError:
+            pass
+'''
+    #popup.display(jsonStr)
 
 if __name__ == "__main__":
     main(sys.argv)
